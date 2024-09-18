06da2be9a5a3607dc6c615ccd0f2c693c8955f34
Abdallah Ahmed Oraby <71422748+abdallahoraby@users.noreply.github.com>
2024-09-18 12:38:39 +0300

    update slider library

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,29 +55,108 @@ jQuery('.filter-menu').on('click', function() {
 });
 
 
-$('.courses-slider').owlCarousel({
-    loop:false,
-    items:3,
-    margin:20,
-    responsiveClass:true,
-    nav: true,
-    dots: false,
-    navText: [
-        '<i class="fas fa-chevron-left"></i>',
-        '<i class="fas fa-chevron-right"></i>'
-    ],
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+
+// splide default options
+let splideOptions = {
+    type   : 'slide',  // Enable looping
+    perPage: 4,       // Show 4 items at once
+    perMove: 1,
+    autoplay: false, // Enable autoplay
+    interval: 3000,   // Time between slides (3 seconds)
+    gap     : '1rem', // Space between slides
+    pagination: false, // Disable pagination
+    arrows: true,      // Enable navigation arrows
+    speed: 300,
+    height: 250,
+    paginationDirection: 'ltr',
+    heightRatio: 0.14,
+    easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
+    drag: true,
+    pauseOnHover: true,
+    lazyLoad: 'sequential',
+    breakpoints: {
+        0: {
+            perPage: 1,
         },
-        600:{
-            items:3,
-            nav:true
+        640: {
+            perPage: 1,
         },
-        1200:{
-            items:4,
-            nav:true,
+        1600: {
+            perPage: 4,
         }
     }
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    if( jQuery('.islamic.splide').length ){
+        new Splide('.islamic.splide', splideOptions).mount();
+    }
+
+    if( jQuery('.quran.splide').length ){
+        new Splide('.quran.splide', {
+            type   : 'slide',
+            perPage: 4,
+            perMove: 1,
+            autoplay: false,
+            interval: 3000,
+            gap     : '1rem',
+            pagination: false,
+            arrows: true,
+            paginationDirection: 'rtl',
+            speed: 300,
+            heightRatio: 0.14,
+            easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
+            drag: true,
+            pauseOnHover: true,
+            lazyLoad: 'sequential',
+            breakpoints: {
+                0: {
+                    perPage: 1,
+                },
+                640: {
+                    perPage: 1,
+                },
+                1600: {
+                    perPage: 4,
+                }
+            }
+        }).mount();
+    }
+
+
 });
+
+/* start tabs function */
+$(function() {
+    "use strict";
+
+    $('a[href*="#"]').on('click', function(event) {
+        {
+            // Figure out element to scroll to
+            var target = $(this);
+            target = target.length ? target : $('[name=' + this.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+}); /* End tabs function */
